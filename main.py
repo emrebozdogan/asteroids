683cc0f95c453a096b898ca0bef3827c25740616
emrebozdogan <emrbzdgn@icloud.com>
2025-01-13 17:51:03 +0300

    Added map and added player to the middle

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,8 +18,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
-        screen.fill()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill(color= "black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = pygame.time.Clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
